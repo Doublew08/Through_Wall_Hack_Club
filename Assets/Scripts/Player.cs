44fb8ec8a330a2064c9f7623270dc0da50b4fc71
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2022-07-18 02:26:04 +0200

    Session 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 public class Player : MonoBehaviour
 {
+    //[SerializeField] private float moveSpeed = 0f;
+    internal static Player instance = null; 
+    internal static Direction direction = Direction.None;
+    internal bool isGameOver;
+    internal bool isGameStarted;
     [SerializeField] private float moveSpeed = 0f;
-    internal static Player instance = null;
-
     [SerializeField] internal SkinnedMeshRenderer playerMeshRenderer = null;
+    [SerializeField] internal Animator animator = null;
+    [SerializeField] internal TrackManager activeTrackManager = null;
+    private MeshRenderer activeGate = null;
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,9 +34,54 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+        if (isGameStarted)
+        {
+            
+        }
+        if (direction == Direction.Left)
+        {
+            transform.Translate(Vector3.left*Time.deltaTime*moveSpeed);
+        }
+        else if (direction == Direction.Right)
+        {
+            transform.Translate(Vector3.right*Time.deltaTime*moveSpeed);
+        }
+        this.gameObject.transform.rotation =  quaternion.identity;
+        //  transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
     }
-    
-    
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Gate")
+        {
+            other.gameObject.transform.parent.transform.parent.gameObject.GetComponent<TrackManager>().BurstGate(other.gameObject.transform.position,other.gameObject.GetComponent<MeshRenderer>().material);
+          //  Destroy(other.gameObject);
+            activeGate = other.gameObject.GetComponent<MeshRenderer>();
+            activeGate.enabled = false;
+            UIManager.instance.gameScore++;
+        }
+        else if (other.gameObject.tag == "Track")
+        {
+            activeTrackManager = other.gameObject.GetComponent<TrackManager>();
+            activeTrackManager.colorChanger();
+        }
+        else if (other.gameObject.tag == "Border" || other.gameObject.tag == "Wall")
+        {
+          //  int val = ;
+            int val = Random.Range(1,3);
+           // print(val);
+            animator.SetInteger("Die#",val);
+            moveSpeed = 0;
+            isGameOver = true;
+            UIManager.instance.GameOver();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Track")
+        {
+            activeGate.enabled = true;
+        }
+    }
 }
--- Assets/Scripts/TrackManager.cs
+++ Assets/Scripts/TrackManager.cs
@@ -6,17 +6,16 @@ using UnityEngine;
 public class TrackManager : MonoBehaviour
 {
     [SerializeField] private Transform trackEnd;
-
     [SerializeField] private Transform trackBegin;
-
     [SerializeField] private float moveSpeed = 0f;
     [SerializeField] private GameObject wallHolder = null;
     [SerializeField] private List<Material> colors = new List<Material>();
-
+    [SerializeField] private ParticleSystem burstEffect;
+    [SerializeField] internal TrackManager nextTrackManagerRef = null;
     private Vector3 trackEndPos = Vector3.zero;
     private Vector3 trackPos = Vector3.zero;
     private List<MeshRenderer> wallRenderers = new List<MeshRenderer>();
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,26 +26,38 @@ public class TrackManager : MonoBehaviour
             wallRenderers.Add(wallHolder.transform.GetChild(i).gameObject.GetComponent<MeshRenderer>());
             
         }
-        colorChanger(); 
+
+        if (Player.instance.activeTrackManager == this)
+        {
+            colorChanger();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-        trackPos = transform.position;
-
-        if (trackPos.z <= trackEndPos.z)
+        if (!Player.instance.isGameOver && Player.instance.isGameStarted)
         {
-            transform.position = new Vector3(0, 0, trackBegin.position.z);
+
+
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+            trackPos = transform.position;
+
+            if (trackPos.z <= trackEndPos.z)
+            {
+                transform.position = new Vector3(0, 0, trackBegin.position.z);
+            }
         }
     }
 
-    private void colorChanger()
+    internal void colorChanger()
     {
         List<Material> temp = new List<Material>();
-        temp = colors;
-        int index;
+        foreach (Material m in colors)
+        {
+            temp.Add(m);
+        }
+        int index = -1;
         foreach (MeshRenderer r in wallRenderers)
         {
             index = Random.Range(0, temp.Count);
@@ -54,9 +65,16 @@ public class TrackManager : MonoBehaviour
             r.gameObject.tag = "Wall";
             temp.RemoveAt(index);
         }
-
        GameObject gate = wallRenderers[Random.Range(0, wallRenderers.Count)].gameObject;
        gate.tag = "Gate";
+       gate.GetComponent<BoxCollider>().isTrigger = true;
        Player.instance.playerMeshRenderer.material = gate.GetComponent<MeshRenderer>().material;
     }
+
+    internal void BurstGate(Vector3 pos,Material mat)
+    {
+        burstEffect.transform.position = pos;
+        burstEffect.Play(true);
+        burstEffect.GetComponent<ParticleSystemRenderer>().material = mat;
+    } 
 }
